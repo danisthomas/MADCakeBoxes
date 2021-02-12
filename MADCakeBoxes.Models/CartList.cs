ffe356674a732d5acc427888bbb23343dc3e70ef
Michael Gilbert <sonic7live@yahoo.com>
2021-02-12 18:45:59 -0500

    Model Push

---
+++ MADCakeBoxes.Models/CartList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MADCakeBoxes.Models
+{
+    public class CartList
+    {
+        private Cake _Cake = new Cake();
+        private GiftBox _GiftBox = new GiftBox();
+
+        [Key]
+        public int CartId { get; set; }
+        public int? ItemCount { get; set; }
+        public double TotalCost
+        {
+            get
+            {
+                return _Cake.CakeCost + _GiftBox.GiftBoxCost;
+
+            }
+        }
+        public DateTime PurchaseDate { get; set; }
+
+        [ForeignKey(nameof(Customer))]
+        [Required]
+        public int CustomerId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+
+        [ForeignKey(nameof(GiftBox))]
+        [Required]
+        public int? GiftBoxId { get; set; }
+        public virtual GiftBox GiftBox { get; set; }
+
+        [Required]
+        public Guid CartUser { get; set; }
+    }
+}
---
+++ MADCakeBoxes.Models/CustomerById.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MADCakeBoxes.Models
+{
+    public class CustomerById
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return ($"{FirstName} {LastName}");
+            }
+        }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public Guid User { get; set; }
+    }
+}
