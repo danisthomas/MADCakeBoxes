81c60e35cc3cb6a48001131beae8e3dc96fe359e
Ashley Sharpe <ashleysharpe00@gmail.com>
2021-02-15 08:33:02 -0600

    Added CakeCreate , Added Guid to Cake so GetCakes method worked.

--- MADCakeBoxes.Models/CakeCreate.cs
+++ MADCakeBoxes.Models/CakeCreate.cs
@@ -5,17 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MADCakeBoxes.Data
+namespace MADCakeBoxes.Models
 {
-    public class Cake
+    public class CakeCreate
     {
-        [Key]
-        public int CakeId { get; set; }
-        public Guid UserId { get; set; }
         [Required]
         public string Flavor { get; set; }
         [Required]
         public string Toppings { get; set; }
-        public double CakeCost { get { return 30.00; } }
+        
     }
 }
